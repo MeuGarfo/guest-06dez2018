bc699be8275a34478140c46e1d0c4d88e3df1341
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-11-23 10:51:51 -0200

    M  view/home.php M  view/menu/mensagem.php

--- view/menu/mensagem.php
+++ view/menu/mensagem.php
@@ -1,7 +1,7 @@
 <div class="btn-group pull-right">
   <button class="btn btn-mini dropdown-toggle" data-toggle="dropdown">Opções <span class="caret"></span></button>
   <ul class="dropdown-menu">
-    <li><a onclick="javascript:editarMensagem(<?php print $id; ?>)" href="javascript:void();">Editar</a></li>
-    <li><a onclick="javascript:apagarMensagem(<?php print $id; ?>)" href="javascript:void();">Apagar</a></li>
+    <li><a onclick="javascript:editarMensagem(<?php print $id; ?>)" href="javascript:void(0);">Editar</a></li>
+    <li><a onclick="javascript:apagarMensagem(<?php print $id; ?>)" href="javascript:void(0);">Apagar</a></li>
   </ul>
 </div><!-- btn-group -->
